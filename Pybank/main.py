fab2cdbe0f468a5a5958de2021c664940c377da2
Fareskazemi <Fareskazemi@live.com.au>
2022-04-01 14:05:08 +0800

    Update main.py

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,4 +1,21 @@
+#import modules
 import os
-import csv
+import csv 
 
-csvpath = os.path.join("Resources","budget_data.csv")
+#import path to CSV and READ
+csvpath=os.path.join('Pybank', 'Resources', 'budget_data.csv')
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    #working directory
+    csv_header = next(csvreader)
+    month = []
+    revenue = []
+    revenue_change = []
+    monthly_chnage = []   
+    print(f"Header: {csv_header}") 
+
+   
+
+
+
